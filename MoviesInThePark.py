ff435b5d4fedab79a882533ad9284dab8a111f1b
Yong Cho <yongcho822@gmail.com>
2015-05-21 22:12:14 -0500

    fixed

--- MoviesInThePark.py
+++ MoviesInThePark.py
@@ -1,5 +1,6 @@
+#Only thing is having to manually remove the out of format HTML Editor's note on Aug 19th Dreamgirls. Typical Dreamgirls.
+
 import csv
-import re
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
@@ -19,21 +20,21 @@ with open("MovieParks.tsv", "w") as f:
         Date = row.text.encode('utf-8')
         master_dict['Date'] = Date
         for sibling in list(row.next_siblings)[:-1]:
-            if sibling.name == "strong":
-                
+            if sibling.name == "strong":                
                 break
             if sibling.name == "em":
                 MovieTitle = sibling.text.encode('utf-8')
                 master_dict['Movie Title'] = MovieTitle
-            if 'Location:' in sibling:
-                Location = sibling.replace("Location: ","") + ", Chicago"
-                master_dict['Location'] = Location.encode('utf-8')
-            if 'Amenities:' in sibling:
-                #not every item has Amenities listed
-                Amenities = sibling.replace("Amenities: ","")
-                master_dict['Amenities'] = Amenities.encode('utf-8')
-
-        writer.writerow(master_dict)
-            
+                for sibling2 in sibling.next_siblings:
+                    if sibling2.name == "strong" or sibling2.name == "em":
+                        break
+                    if 'Location:' in sibling2:     
+                        Location = sibling2.replace("Location: ","") + ", Chicago "
+                        master_dict['Location'] = Location.encode('utf-8')
+                    if 'Amenities:' in sibling2:
+                        Amenities = sibling2.replace("Amenities: ","")
+                        master_dict['Amenities'] = Amenities.encode('utf-8')
+                writer.writerow(master_dict)
+                master_dict = {}
+                master_dict['Date'] = Date   
 print 'Done here'
-
