e646c9c42f104de0c0cd00e064205396eff0b901
Yong Cho <yongcho822@gmail.com>
2015-05-22 16:01:58 -0500

    script to geocode TSV File after initial MovesInThePark script

--- GeocodeScript.py
+++ GeocodeScript.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[8]:
-
-#LETS GEOCODE THIS SHIIIIIIT
 from urllib2 import urlopen
 import csv
 import json
@@ -12,13 +6,13 @@ from time import sleep
 def geocode(address):
     url = ("http://maps.googleapis.com/maps/api/geocode/json?"
         "sensor=false&address={0}".format(address.strip().replace(" ", "+")))
-    #print url
+    print url
     return json.loads(urlopen(url).read())
 
-with open("MovieParksFixed.tsv", "rU") as f:
+with open("MovieParksTest.tsv", "rU") as f:
     reader = csv.DictReader(f, delimiter = "\t")
     
-    with open("MovieParksGeocode2.tsv","w") as w:
+    with open("MovieParksGeocodeTest.tsv","w") as w:
         fields = ['Location', 'Movie Title', 'Date', 'Amenities', 'Formatted_Address', 'Lat', 'Lng']
         writer = csv.DictWriter(w, fieldnames = fields, delimiter = "\t")
         writer.writeheader()
@@ -26,7 +20,7 @@ with open("MovieParksFixed.tsv", "rU") as f:
         for line in reader:
             print "Geocoding: {0}".format(line['Location'])
             response = geocode(line['Location'])
-            print "got response"
+            
             if "OK" in response['status']:
                 #print "response ok"
                 results = response.get("results")[0]
@@ -34,51 +28,10 @@ with open("MovieParksFixed.tsv", "rU") as f:
                 line['Lat'] = results["geometry"]["location"]["lat"]
                 line['Lng'] = results["geometry"]["location"]["lng"]
             else:
-                #print "not ok"
+                print "NOOOOT OOOOOKKKKKK"
                 line['Formatted_Address'] = ""
                 line['Lat'] = ""
                 line['Lng'] = ""
             sleep(1)
             writer.writerow(line)
 print "Done here."
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
